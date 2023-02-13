1bf67a057e8dfe8b87da2b272b6973a929420e96
VitaliyYurkevich <vitaliy.yurkevich@yandex.ru>
2023-02-13 23:35:33 +0300

    upd

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -23,7 +23,7 @@ function TodoList(props: TodolistType)  {
             <h3>{props.title}</h3>
             <div>
                 <input/>
-                <button>+</button>
+                <button onClick={()=>{props.AddTask}}>+</button>
             </div>
             <ul>
                 {props.tasks.map((t)=>{
