737a09b020bce19b630c044151eb7edeb882fb0c
VitaliyYurkevich <vitaliy.yurkevich@yandex.ru>
2023-02-20 23:17:49 +0300

    upd

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ function App() {
         let newTask = [...tasks, task]
         setTasks(newTask)
     }
-
+    console.log('asda')
 
     let FilteredTask = (value:FilterType) => {
         setFiltered(value)
