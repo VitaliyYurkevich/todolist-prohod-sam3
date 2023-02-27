d4a61006d9837686d3aa7e4d0877ed7000685c7c
VitaliyYurkevich <vitaliy.yurkevich@yandex.ru>
2023-02-27 23:19:34 +0300

    upd

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ function App() {
         {id: v1(), title: 'JS', isDone: true},
         {id: v1(), title: 'React', isDone: false}
     ])
-
+    console.log('sdfsf')
     let [filtered, setFiltered] = useState<FilterType>('all')
 
     let AddTask = (title:string) => {
